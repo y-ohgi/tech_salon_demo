08a8aff8290c043279d540b277500a838228c7ed
kyuns <kyun0612@gmail.com>
2016-09-08 01:41:29 +0900

    Itemにget_item,get_itemsを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,10 +14,17 @@ class ItemsController < ApplicationController
 
   def show
     item_id = params[:id]
-    @item = Item.find_by(id: item_id)
+
+    # Redisに置き換える
+    #@item = Item.find_by(id: item_id)
+    @item = Item.get_item(item_id)
+
     like_user_ids = LikeItem.where(item_id: item_id).order("id desc").limit(100).pluck(:user_id)
     @users = User.where(id: like_user_ids)
     item_ids = Item.where(brand_id: @item['brand_id']).order("id desc").limit(20).pluck(:id)
-    @items = Item.where(id: item_ids)
+
+    # Redisに置き換える
+    # @items = Item.where(id: item_ids)
+    @items = Item.get_items(item_ids)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,25 @@
 class Item < ApplicationRecord
+
+  @@cache = RedisCache.new(self.table_name.to_sym)
+
+  def self.get_item(item_id)
+    item = @@cache.get(item_id)
+    if !item.nil?
+      return item
+    end
+    item = Item.find_by(id: item_id)
+    @@cache.set(item_id, item)
+    item
+  end
+
+  def self.get_items(item_ids = [])
+    items = @@cache.bulk_get(item_ids)
+    item_ids.each_with_index do |id, idx|
+      if items[idx].nil?
+        items[idx] = self.get_item(id)
+      end
+    end
+    items
+  end
+
 end
