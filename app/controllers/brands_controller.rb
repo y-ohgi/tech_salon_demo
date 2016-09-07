eff2f667ff416928584ea8f69266b037cbd1dcb7
kyuns <kyun0612@gmail.com>
2016-09-08 03:01:46 +0900

    fix templates

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,7 +1,7 @@
 class BrandsController < ApplicationController
   def index
     # アイテムの数が多いブランド順
-    brand_id_counts = Item.where.not(:brand_id => 0).group(:brand_id).order('count_brand_id desc').limit(10).count('brand_id')
+    brand_id_counts = Item.where.not(:brand_id => 0).group(:brand_id).order('count_brand_id desc').limit(100).count('brand_id')
 
     @brands = []
     Brand.where(id: brand_id_counts.keys).each do |brand|
@@ -16,7 +16,7 @@ class BrandsController < ApplicationController
     brand_id = params[:id]
     @brand = Brand.find_by(id: brand_id)
     @items  = Item.where(brand_id: brand_id).limit(100)
-    like_user_ids = LikeBrand.where(brand_id: brand_id).pluck(:user_id)
+    like_user_ids = LikeBrand.where(brand_id: brand_id).limit(100).pluck(:user_id)
     @users   = User.where(id: like_user_ids)
   end
 end
